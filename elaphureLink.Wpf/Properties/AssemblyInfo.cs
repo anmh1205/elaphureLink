cc593edb3b826f3e3e71a82c43c2d2efb7ed9d6a
Le Trong An <91620228+anmh1205@users.noreply.github.com>
2025-05-29 19:24:45 +0700

    [assembly: AssemblyVersion("1.0.0.0")] [assembly: AssemblyFileVersion("1.0.0.0")]

--- elaphureLink.Wpf/Properties/AssemblyInfo.cs
+++ elaphureLink.Wpf/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Windows;
 
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion(ThisAssembly.Git.BaseTag)]
-[assembly: AssemblyFileVersion(ThisAssembly.Git.BaseTag)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
